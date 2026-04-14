badb3a733b02c0375c02379c0389fe1c75412495
mohammadasadi67 <mohammadasadollahzadeh67@gmail.com>
2026-04-14 12:37:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ for col, t in zip(header, titles):
 # ======================
 for i, (idx, row) in enumerate(rows):
 
-    # ✅ کلید یکتا (رفع کامل ارور)
+    # ✅ کلید یکتا (حل کامل ارور)
     key = f"trade_{i}_{idx.strftime('%Y%m%d%H%M')}"
 
     cols = st.columns([2,1,1,1,1,1,1,1,1,1])
